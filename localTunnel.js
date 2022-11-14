46382d5278efbb2ceb248c2dce7ff03ff1ed659d
VdovinAV <aleksandr.vdovin@vseinstrumenti.ru>
2022-11-14 20:57:25 +0300

    * | localTunnel fix

--- localTunnel.js
+++ localTunnel.js
@@ -1,5 +1,5 @@
 const localTunnel = require('localtunnel');
 
-await localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot" }, function(err, tunnel) {
+localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot" }, function(err, tunnel) {
     console.log('localTunnel running')
 });
