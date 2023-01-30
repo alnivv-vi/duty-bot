c926c022a1b8c600921e4d8f26d3413b1a6d0763
VdovinAV <aleksandr.vdovin@vseinstrumenti.ru>
2023-01-30 17:52:46 +0300

    * | lt subdomain

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.message(/(\/failedRerunTests\.txt).*/, async ({ context,message, say }) => {
 (async () => {
     console.log('⚡️duty-bot готов к работе ⚡');
     await googleDocService.start();
-    await localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot" }, function(err, tunnel) {
+    await localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot5" }, function(err, tunnel) {
             console.log('localTunnel running')
         });
     await app.start(process.env.PORT || 3000);
