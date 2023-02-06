be6fcaa78af30afd67da6dee735c670e3cc3c6bb
VdovinAV <aleksandr.vdovin@vseinstrumenti.ru>
2023-02-06 09:44:14 +0300

    * | lt down

--- index.js
+++ index.js
@@ -88,8 +88,8 @@ app.command('/flaky', async ({command, ack, say}) => {
 (async () => {
     console.log('⚡️duty-bot готов к работе ⚡');
     await googleDocService.start();
-    await localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot5" }, function(err, tunnel) {
-            console.log('localTunnel running')
-        });
+    // await localTunnel(process.env.PORT || 3000, { subdomain: "vi-duty-bot5" }, function(err, tunnel) {
+    //         console.log('localTunnel running')
+    //     });
     await app.start(process.env.PORT || 3000);
 })();
